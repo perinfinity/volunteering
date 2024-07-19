63f143764b7c375b800b896bddfa751db63fce88
ntepp <ntepp.marcus@gmail.com>
2024-07-19 21:36:03 +0200

    update readme and initial opportunity-list

--- src/app/opportunity/services/opportunity.service.spec.ts
+++ src/app/opportunity/services/opportunity.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { OpportunityService } from './opportunity.service';
 
@@ -6,7 +7,10 @@ describe('OpportunityService', () => {
   let service: OpportunityService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [OpportunityService]
+    });
     service = TestBed.inject(OpportunityService);
   });
 
