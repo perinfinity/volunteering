e8c3126b8f52618624eae53051eff5c8a3052d20
smakn <116876737+smakn@users.noreply.github.com>
2024-08-19 21:54:35 +0100

    nouvelle fonctionalite register form

---
+++ src/app/auth/register/service/registration.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../../../models/user.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+  private apiUrl = 'http://localhost:3000/api/auth/register';
+
+  constructor(private http: HttpClient) { }
+
+  register(user: User): Observable<any> {
+    return this.http.post<any>(this.apiUrl, {
+      identifiant: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      email: user.email,
+      password: user.password,
+      role: user.role,
+      phone: user.phone,
+      org_name : user.org_name,
+      bio : user.bio,
+      adresse : user.address,
+      create_date : user.created_at,
+      updated_date : user.updated_at
+    });
+  }
+}
